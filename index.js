f389523fcb6f1cd3850d0504f6e40630698c6e53
Mihir Solanki <127624978+MihirSolankii@users.noreply.github.com>
2025-06-01 21:22:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { exec } from 'child_process';
-import fs from 'fs';
-import Problem from './models/problem.js'; // Mongoose model
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import { exec } from 'child_process';
+// import fs from 'fs';
+// import Problem from './models/problem.js'; // Mongoose model
 
-const app = express();
-app.use(express.json()); // Use this instead of bodyParser.json()
-
-
+// const app = express();
+// app.use(express.json()); // Use this instead of bodyParser.json()
 
 
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// const PORT = 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
